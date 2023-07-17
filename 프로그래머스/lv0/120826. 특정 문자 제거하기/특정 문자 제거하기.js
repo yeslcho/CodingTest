e245a8d72463d32be6d1ca3cd03ecec6c7fcfdf5
yeslcho <138556042+yeslcho@users.noreply.github.com>
2023-07-17 23:11:06 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -0,0 +1,12 @@
+function solution(my_string, letter) {
+    var answer = '';
+    let cnt = 0;
+    while(cnt < my_string.length) {
+        if(my_string[cnt] !==letter) {
+            answer = answer + my_string[cnt]
+        }
+        cnt = cnt + 1;
+        
+    }
+    return answer;
+}
