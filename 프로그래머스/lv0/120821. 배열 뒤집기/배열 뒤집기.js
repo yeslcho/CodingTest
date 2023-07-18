2d102f33d9efc444a037d8d3675ef64ac79e2c9b
yeslcho <138556042+yeslcho@users.noreply.github.com>
2023-07-18 22:46:00 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    
+    const newArray = []
+    let cnt = num_list.length - 1;
+    while(cnt >= 0) {
+        newArray.push(num_list[cnt]);
+        cnt = cnt - 1;
+    }
+   return newArray;
+}
