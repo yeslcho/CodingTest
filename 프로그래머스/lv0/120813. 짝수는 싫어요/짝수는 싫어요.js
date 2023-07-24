5bf6412322994caa94a88a37192cfea88f1f172b
yeslcho <138556042+yeslcho@users.noreply.github.com>
2023-07-24 21:50:38 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    var answer = [];
+    let cnt = 1; 
+    while(cnt <= n) {
+        if(cnt % 2 === 1) {
+            answer.push(cnt);
+        }
+        cnt = cnt + 1;
+        
+    }
+    return answer;
+}
