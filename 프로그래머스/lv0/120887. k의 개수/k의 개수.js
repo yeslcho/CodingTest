ef7ba694d29e5f198e9a11ac7342dca693fcc318
yeslcho <138556042+yeslcho@users.noreply.github.com>
2023-08-29 00:49:18 +0900

    [level 0] Title: k의 개수, Time: 10.52 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.js
@@ -1,8 +1,12 @@
-function solution(i, j, k) {
-    let a ='';
-    for(i; i<=j; i++){
-        a += i;
+const solution = (i, j, k) => {
+  let result = 0;
+  for (let num = i; num <= j; num++) {
+    const split = String(num).split('');
+    for (let num2 = 0; num2 < split.length; num2++) {
+      if (split[num2].includes(String(k))) {
+        result++;
+      }
     }
-
-    return a.split(k).length-1;
+  }
+  return result;
 }
