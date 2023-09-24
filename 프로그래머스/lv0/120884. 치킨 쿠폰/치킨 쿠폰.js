ffd099c625d6ca13c33b9d60f6232f96f77e9b81
yeslcho <138556042+yeslcho@users.noreply.github.com>
2023-09-24 15:07:57 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -0,0 +1,10 @@
+function solution(chicken) {
+    let answer = 0; 
+    
+    while(chicken >= 10) {
+        answer += Math.floor(chicken / 10);
+        chicken = Math.floor((chicken / 10) + ( chicken % 10));
+    }
+    
+    return answer;
+}
