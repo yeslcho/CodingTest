d2e0d9939f7da5c54d201c45902d673a7450799e
yeslcho <138556042+yeslcho@users.noreply.github.com>
2023-10-07 22:08:42 +0900

    [level 0] Title: 다항식 더하기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.js
@@ -0,0 +1,31 @@
+function solution(polynomial) {
+    const values = polynomial.split(' + ');
+    let x = 0;
+    let c = 0;
+    for(let i=0; i<values.length; i++){
+        const item = values[i];
+        if(item[item.length-1] !== 'x'){
+            c += Number(item);
+        } else {
+            const num = item.split('x')[0];
+            if(num === ''){
+                x += 1;
+            } else {
+               x += Number(num); 
+            }
+        }
+    }
+    let answer = '';
+    if(x === 1){
+        answer += 'x';
+    }
+    if(x > 1){
+        answer += `${x}x`;
+    }
+    if(x === 0 && c > 0){
+        answer += c;
+    } else if(c > 0){
+        answer += ` + ${c}`;
+    }
+    return answer;
+}
