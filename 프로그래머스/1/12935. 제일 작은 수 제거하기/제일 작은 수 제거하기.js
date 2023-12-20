0e162208cb2bf37142d2b116ba5022af48a6c38b
yeslcho <138556042+yeslcho@users.noreply.github.com>
2023-12-20 11:43:03 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.05 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -0,0 +1,17 @@
+function solution(arr) {
+    
+    
+    var answer = [];
+ 
+    
+    if (arr.length<=1){
+        return [-1];
+
+    }else{
+        
+        arr.splice(arr.indexOf(Math.min(...arr)),1);
+        answer =  arr;
+    }   
+    
+    return answer
+}
