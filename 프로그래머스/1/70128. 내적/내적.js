e3b8c3a692c5cc7c8b5fc7c8d4be092b4621e0e8
yeslcho <138556042+yeslcho@users.noreply.github.com>
2024-01-11 14:43:35 +0900

    [level 1] Title: 내적, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,7 +1,7 @@
 function solution(a, b) {
-    let result = 0;
-    for (let i in a) {
-        result += a[i] * b[i]
-    }
-    return result
+  let result = 0;
+  for (let i = 0; i < a.length; i++) {
+    result += a[i] * b[i];
+  }
+  return result;
 }
