54b06b4958f4a9a48952278d552fc4a13cd5413a
yeslcho <138556042+yeslcho@users.noreply.github.com>
2024-01-11 10:57:28 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,7 +1,7 @@
 function solution(s) {
     var answer = '';
     
-    if (s.length % 2 == 0 ) { 
+    if (s.length % 2 === 0 ) { 
         answer = s[s.length / 2 - 1] + s[s.length / 2];
     } else {
         answer = s[Math.floor(s.length/2)];
