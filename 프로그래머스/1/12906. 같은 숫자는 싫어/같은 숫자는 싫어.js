64116c6b67d3da432f54933a688d881b37642b4f
yeslcho <138556042+yeslcho@users.noreply.github.com>
2024-01-25 22:40:58 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 55.58 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,9 +1,16 @@
-function solution(arr){
-    var answer = [];
-    for(var i = 0; i < arr.length; i++){
-        if(arr[i] !== arr[i + 1]){
-           answer.push(arr[i]);
-        }        
+function solution(arr) {
+    // 스택을 초기화합니다.
+    const stack = [];
+    
+    // 주어진 배열을 순차적으로 탐색합니다.
+    for (let num of arr) {
+        // 스택이 비어있지 않고, 스택의 마지막 원소가 현재 숫자와 같지 않다면
+        // 스택에 현재 숫자를 추가합니다.
+        if (stack.length === 0 || stack[stack.length - 1] !== num) {
+            stack.push(num);
+        }
     }
-    return answer;
+    
+    // 스택에 남아있는 원소가 결과입니다.
+    return stack;
 }
