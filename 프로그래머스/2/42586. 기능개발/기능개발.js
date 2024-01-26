061ab3822184c9c332e1acd68683dca70574ed7b
yeslcho <138556042+yeslcho@users.noreply.github.com>
2024-01-26 14:14:50 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -0,0 +1,19 @@
+function solution(progresses, speeds) {
+    let answer = [];
+    let days = progresses.map((progress, index) => Math.ceil((100 - progress) / speeds[index]));
+    let maxDay = days[0];
+
+    let count = 0;
+    for (let i = 0; i < days.length; i++) {
+        if (days[i] <= maxDay) {
+            count++;
+        } else {
+            answer.push(count);
+            count = 1;
+            maxDay = days[i];
+        }
+    }
+    answer.push(count);
+
+    return answer;
+}
