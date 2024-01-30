0be4cd1e8b4d7e8136ab1df1b4c73215e2507238
yeslcho <138556042+yeslcho@users.noreply.github.com>
2024-01-30 16:04:49 +0900

    [level 2] Title: 가장 큰 수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,13 +1,4 @@
 function solution(numbers) {
-    // 1. 숫자를 문자열로 변환
-    var answer = numbers.map(number => number.toString());
-    
-    // 2. 정렬
-    answer.sort((a, b) => (b + a) - (a + b));
-    
-    // 3. 합치기
-    answer = answer.join('');
-    
-    // 4. 0 처리
+    let answer = numbers.map(number => number.toString()).sort((a, b) => (b + a) - (a + b)).join('');
     return answer[0] === '0' ? '0' : answer;
 }
