ab5c8a4093c4af50ceaa0ca8d64f564129ea26ca
yeslcho <138556042+yeslcho@users.noreply.github.com>
2024-02-01 22:30:10 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,11 +1,12 @@
 function solution(array, commands) {
     var answer = [];
-    for (let i = 0; i < commands.length; i++){
-        let selectedArray = array.slice(commands[i][0] - 1, commands[i][1]);
+    
+    for(var i=0; i<commands.length;i++){
+        var list = array.slice(commands[i][0]-1, commands[i]
+        							[1]).sort((a,b)=>{return a-b});
         
-        selectedArray.sort((a,b)=>a-b);
-        
-        answer.push(selectedArray[commands[i][2] - 1]);
+        answer.push(list[commands[i][2]-1]);
     }
+
     return answer;
 }
